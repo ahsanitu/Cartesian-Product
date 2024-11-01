8bbe6b81df841712c0bd26e2f6e64985d860a41b
Ahsan <bscs24014@itu.edu.pk>
2024-11-01 18:54:45 +0500

    Input two sets and show them

--- main.cpp
+++ main.cpp
@@ -22,14 +22,49 @@ int main(){
             cout<<"Enter the elements of set A : ";
             inputArray(setA,size_A);                
 
+            cout<<endl;
+
             cout<<"Enter the number of elements in set B : ";
             int size_B;
             cin>>size_B;
-
+            cout<<endl;
+            
             int* setB = new int[size_B];    // allocating memory at runtime and assigning the address to pointer "setB"
+             cout<<"Enter the elements of set B : ";
+            inputArray(setB, size_B);
+
+            cout<<endl;
+
+            cout<<"Set A = {";
+
+                for(int i=0; i<size_A ; i++ ){
+                    
+                    cout<<setA[i];
+
+                    if(i != size_A - 1)
+                        cout<<",";
+                    
+                }
+
+            cout<<"}";
+
+             cout<<endl;
+
+            cout<<"Set B = {";
+
+                for(int i=0; i<size_B ; i++ ){
+                    
+                    cout<<setB[i];
+
+                    if(i != size_B - 1)
+                        cout<<",";
+                    
+                }
+
+            cout<<"}";
 
 
-     // }
+     // }   
 
 
 
@@ -44,3 +79,4 @@ void inputArray(int arr[],int size){ // function to take input in array
 for(int i = 0 ; i<size ; i++ )
     cin >> arr[i];
 }
+
